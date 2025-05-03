61c53ffe01dd1704dee6818b478e461070db47a5
Cupid Valentine <westerngod@126.com>
2025-05-03 09:10:54 +0800

    chore: xx

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 import { AppService } from '../src/app.service';
 
 describe('AppController (e2e)', () => {
